ee46e75b1ba9ee2590c21ac6965cc465aa0a8bef
eduardoviegas <eduardo.viegas@terras.agr.br>
2022-01-30 10:30:59 -0300

    primeiro commit

---
+++ app/src/main/java/com/innaval/netflxremake/model/Movie.java
@@ -0,0 +1,16 @@
+package com.innaval.netflxremake.model;
+
+public class Movie {
+
+    private String coverUrl;
+
+    public String getCoverUrl() {
+        return coverUrl;
+    }
+
+    public void setCoverUrl(String coverUrl) {
+        this.coverUrl = coverUrl;
+    }
+
+
+}
